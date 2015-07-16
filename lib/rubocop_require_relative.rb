8829b80281072d408a50cf83b6d06107fd9bac77
Max Jacobson <max@hardscrabble.net>
2015-07-16 19:20:20 -0400

    Implement the gem

--- lib/rubocop_require_relative.rb
+++ lib/rubocop_require_relative.rb
@@ -1,5 +1,17 @@
 require "rubocop_require_relative/version"
+require "rubocop"
 
-module RubocopRequireRelative
-  # Your code goes here...
+module RuboCop
+  module Cop
+    module Lint
+      class RequireRelative < Cop
+        def on_send(node)
+          return unless node.to_a[1] == :require_relative
+          msg = "You have a 'require_relative' statement!"
+          add_offense(node, :expression, msg)
+        end
+      end
+    end
+  end
 end
+
